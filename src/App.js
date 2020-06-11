254cf704a94dbf3f75035da1cc704807cddeb24f
collegewap <collegewap@gmail.com>
2020-06-11 09:17:55 +0530

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function App() {
+  const [mode, setMode] = useState("dark");
+
+  const isLight = mode === "light";
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" style={{ background: "#333", height: "100vh" }}>
+      <button
+        onClick={() => {
+          setMode(mode === "light" ? "dark" : "light");
+        }}
+      >
+        Toggle
+      </button>
+      <br />
+      {isLight && <BsMoon size={25} color="#fff" title="Switch to Dark Mode" />}
+      {!isLight && (
+        <BsSun size={25} color="#ff0" title="Switch to Light Mode" />
+      )}
     </div>
   );
 }
